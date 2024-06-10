c720c0e060de260ef23036222b3a1bef776fa910
Dothingan <22010105@st.phenikaa-uni.edu.vn>
2024-06-10 17:01:40 +0700

    update

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,12 @@
-function header__switchModal(showId) {
-    document.getElementById(showId).style.display = 'flex';
-} 
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('registerModal').style.display = 'none';
+    document.getElementById('loginModal').style.display = 'none';
+});
+
+// function header__switchModal(showId) {
+//     document.getElementById(showId).style.display = 'flex';
+// } 
 
 function switchModal(hideId, showId) {
     document.getElementById(hideId).style.display = 'none';
@@ -14,3 +20,11 @@ function closeModal(modalId) {
 function openModal(modalId) {
     document.getElementById(modalId).style.display = 'flex';
 }
+
+function switchPhone(mainId, main1Id) {
+    // Ẩn phần tử với id main
+    document.getElementById(mainId).style.display = 'none';
+    // Hiển thị phần tử với id main1
+    document.getElementById(main1Id).style.display = 'flex';
+}
+
